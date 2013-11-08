e68684eaa1301fce2156c592eb439c307dab0c1d
Stefan Neculai <stefan.neculai@gmail.com>
2013-11-08 20:47:32 +0200

    Update sessions_controller_captcha.rb

--- lib/devise_security_extension/patches/sessions_controller_captcha.rb
+++ lib/devise_security_extension/patches/sessions_controller_captcha.rb
@@ -4,7 +4,6 @@ module DeviseSecurityExtension::Patches
     included do
       define_method :create do
         if (resource_name.to_s.capitalize.constantize.devise_modules.include?(:security_questionable) == false or valid_captcha? params[:captcha]) 
-          puts 'da'
           resource = warden.authenticate!(auth_options)
           set_flash_message(:notice, :signed_in) if is_navigational_format?
           sign_in(resource_name, resource)
